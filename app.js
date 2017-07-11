80a16713ace8034dbb281e23accba65c60903f45
Max Rashes <mrashes2@gmail.com>
2017-07-10 19:35:42 -0500

    it works!

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const config = require('./config.js')
 let T = new Twitter(config)
 
 var params = {
-	q: '#nodejs',
+	q: '#msichicago',
 	count: 10,
 	result_type: 'recent',
 	lang: 'en'
 }
 
-t.get('search/tweets', params, function(err, data, res) {
+T.get('search/tweets', params, function(err, data, res) {
 	if (!err) {
-		console.log(data)
+		// console.log(data)
+		for (let i=0; i<data.statuses.length; i++) {
+			let id = { id: data.statuses[i].id_str }
+			T.post('favorites/create', id, function(err, response){
+				if(err){
+					console.log(err);
+				}
+				else {
+					let username = response.user.screen_name;
+					let tweetId = response.id_str;
+					console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+				}
+			});
+		}
 	}
 	else {
 		console.log(err)
