a5a328341d76b034d7f4d66a7d231573f3f66a81
Max Rashes <mrashes2@gmail.com>
2017-07-15 09:26:33 -0500

    initialize testing branch

--- app.js
+++ app.js
@@ -2,32 +2,67 @@
 const Twitter = require('twitter');
 const config = require('./config.js');
 const argv = require('yargs').argv;
+const stream = require('stream');
 
 //this creates the instance of it working
 let T = new Twitter(config)
 
 //using -q will search for a # on your input
-let query = '#'+argv.q || '#dogs';
+let query = argv.q || '#chicago';
 
 //parameters for the api call
 let params = {
 	q: query,
+	// geocode:'41.7952713,-87.5773205,.2mi',
 	count: 10,
 	result_type: 'recent',
-	lang: 'en'
+	lang: 'en',
+	id: 2379574
+	
 }
 
-//chicago WOEID
-let id = 2379574
+// //chicago WOEID
+// let id = 2379574
 
-T.get('trends/place', id, function(err, data, res){
-	if (!err) {
-		console.log(data)
-	}
-	else {
-		console.log(err)
-	}
-})
+
+//this just displays trends
+function trendTweets() {
+	//use trends/place
+	T.get('trends/place', params, function(err, data, res){
+		//if no error
+		if (!err) {
+			let content = JSON.parse(res.body);
+			// console.log(content[0].trends[i].name)
+			for (i=0; i<content[0].trends.length; i++){
+				console.log(content[0].trends[i].name)
+			}
+		}
+		else {
+			console.log(err)
+		}
+	})
+}
+
+//this displays based on MSI location
+function locationSearch() {
+	T.get('search/tweets', params, function(err, data, res) {
+		if (!err) {
+			// console.log(data)
+			console.log(`looking for tweets at the museum of science and industry that have the word ${params.q} in it`)
+			//show returns
+			// console.log(data)
+			for (let i=0; i<data.statuses.length; i++) {
+				//find the tweets ID
+				console.log(data.statuses[i].text)
+				console.log(' ')
+			}
+		}
+		else {
+			console.log(err)
+		}
+	})
+}
+
 
 //favorite queries function
 function favTweets() {
@@ -59,6 +94,8 @@ function favTweets() {
 		}
 	})
 }
+
+locationSearch()
 
 // favTweets();
 
